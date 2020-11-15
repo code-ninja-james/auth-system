4d931f9b3759fb9f6048f9da81bd4a5f9b8503f1
Jameson <githinjijmaeson@gmail.com>
2020-11-15 18:23:56 +0300

    clientSide

--- server/index.js
+++ server/index.js
@@ -1,30 +1,33 @@
-const express =require('express');
-const mongoose=require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app =express();
+const app = express();
 
 app.use(express.json());
 
-const authRoutes=require('./routes/auth');
-const verifyToken=require('./routes/verifyToken');
+const authRoutes = require("./routes/auth");
+const verifyToken = require("./routes/verifyToken");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the auth system');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the auth system");
+});
 
-app.get('/api/user/profile',verifyToken,(req,res)=>{
-    res.send('This is the user profile')
-})
+app.get("/api/user/profile", verifyToken, (req, res) => {
+  res.send({success:true,data:req.user});
+});
 
-app.use('/api/users', authRoutes);
+app.use("/api/users", authRoutes);
 
-mongoose.connect('mongodb+srv://j33my:PaRwgUhxbwzdV97b@passportauth.eki46.mongodb.net/auth_system?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        app.listen(port, () => console.log('Server is running'));
-    })
-    .catch(err => console.log(err))
-
+mongoose
+  .connect(
+    "mongodb+srv://j33my:PaRwgUhxbwzdV97b@passportauth.eki46.mongodb.net/auth_system?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    app.listen(port, () => console.log("Server is running"));
+  })
+  .catch((err) => console.log(err));
--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -1,20 +1,19 @@
-const jwt=require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const tokenConfig = process.env.TOKEN;
 
-module.exports=function(req,res,next){
-    const token =req.header('auth-token')
-    if(!token)return res.status(401).send('Acess denied');
+module.exports = function (req, res, next) {
+  const token = req.header("auth-token");
+  if (!token) return res.status(401).send("Acess denied");
 
-    //verify token
-    try{
-const verified=jwt.verify(token,tokenConfig)
-req.user=verified;
-next();
-    }catch(error){
-res.status(400).send('Invalid token')
-    }
-
-}
+  //verify token
+  try {
+    const verified = jwt.verify(token, tokenConfig);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(400).send("Invalid token");
+  }
+};
